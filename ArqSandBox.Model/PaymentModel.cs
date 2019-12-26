7e1b8b1c4b689875579933fd45011eb25f87cd6b
Lucas.Hipolito - Prestador <lucas.hipolito@coopersystem.com.br>
2019-12-26 09:54:14 -0300

    Business classes updated

---
+++ ArqSandBox.Model/PaymentModel.cs
@@ -0,0 +1,13 @@
+﻿using ArqSandBox.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArqSandBox.Model
+{
+    public class PaymentModel
+    {
+        public double Total { get; set; }
+        public PaymentTypeEnum PaymentType { get; set; }
+    }
+}
--- ArqSandBox.Model/ServiceModel.cs
+++ ArqSandBox.Model/ServiceModel.cs
@@ -8,7 +8,15 @@ namespace ArqSandBox.Model
     {
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-
         public ServiceProviderModel Provider { get; set; }
+        public CustomerModel Customer { get; set; }
+        public double Total
+        {
+            get
+            {
+                return this.Payment != null ? this.Payment.Total : 0.0;
+            }
+        }
+        public PaymentModel Payment { get; set; }
     }
 }
