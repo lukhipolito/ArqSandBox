ad26b63f200079b41c4adca2f22d444a3e8897ee
hipolito <lucas.hipolito@coopersystem.com.br>
2020-05-18 10:35:12 -0300

    ground zero arqSandbox project

---
+++ ArqSandBox.API/Controllers/SalesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ArqSandBox.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SalesController : ControllerBase
+    {
+        private readonly ILogger<SalesController> _logger;
+
+        public SalesController(ILogger<SalesController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ ArqSandBox.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArqSandBox.Common.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            return Char.IsUpper(str[0]);
+        }
+    }
+}
